923f122c88996b75825113256649aa6fb60fa722
YonY18 <yonap80@gmail.com>
2022-06-25 09:34:27 -0300

    intento 6000

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   mode: "development",
   build: {
     minify: false,
