1f108812e78780b58810a4a5251006b0671290f0
YonY18 <yonap80@gmail.com>
2022-06-29 10:45:28 -0300

    :)

--- src/components/CommonComponents/AllBooks/AllBooks.jsx
+++ src/components/CommonComponents/AllBooks/AllBooks.jsx
@@ -7,8 +7,6 @@ import Loading from "../Loading/Loading"
 import NotFound from "../../CommonComponents/NotFound/NotFound"
 import { cleanData, getBooks } from "../../../redux/actions/actionBooks.js"
 import { formatToCurrency } from "../../../utils/helperFunctions"
-import Chat from "../ChatBot/ChatBot"
-import { TbRobot } from "react-icons/tb"
 import Footer from "../Footer/Footer"
 
 function AllBooks() {
@@ -30,7 +28,6 @@ function AllBooks() {
   const indexOfFirstBooks = indexOfLastBooks - pageSize
   const currentBooks = allBooks?.slice(indexOfFirstBooks, indexOfLastBooks)
   const [loading, setLoading] = useState(true)
-  const [chatbot, setChatbot] = useState(false)
 
   if (allBooks?.length > 0 && loading) {
     setTimeout(() => {
@@ -73,18 +70,6 @@ function AllBooks() {
           ) : (
             <NotFound />
           )}
-          <div className='contanedorChat'>
-            {
-              chatbot ?
-                <div className='visible'><Chat setChatbot={setChatbot} chatbot={chatbot} /></div> : null
-            }
-
-            <abbr title="NECESITAS AYUDA..">
-              <button onClick={() => setChatbot(!chatbot)}
-                className="link-chatbot"><TbRobot className="robot" />
-              </button></abbr>.
-
-          </div>
           <Pagination
             pageSize={pageSize}
             pageCurrent={pageCurrent}
